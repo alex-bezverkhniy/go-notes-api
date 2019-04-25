0f3793445c46fdfbf0a2009069d6efb0d83c3980
Alexandr Bezverkhniy <alexandr.bezverkhniy@sap.com>
2019-04-25 12:50:12 -0500

    Adde running logic

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 func main() {
-	a := App{}
-	// TODO: move args into enveronments
-	a.Initialize("gonotes", "1Q2w3e4r", "gonotes")
+	a := NewApp("gonotes", "1Q2w3e4r", "gonotes")
 	a.Run(":8080")
 }
