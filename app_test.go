d7947e60c09e37361025773777d864bb4cca7ad7
Alexandr Bezverkhniy <alexandr.bezverkhniy@sap.com>
2019-04-25 12:49:52 -0500

    Added tests

---
+++ app_test.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/alex-bezverkhniy/go-notes-api/model"
+	"github.com/alex-bezverkhniy/go-notes-api/respositories"
+)
+
+var a App
+var nr respositories.NoteRepository
+
+func TestMain(m *testing.M) {
+	a = NewApp("gonotes", "1Q2w3e4r", "gonotes")
+
+	// a.NoteRepository.EnsureNotesTableExists()
+	prepareNotes(a.NoteRepository)
+	code := m.Run()
+	a.NoteRepository.ClearNotesTable()
+
+	os.Exit(code)
+}
+
+func TestNotes(t *testing.T) {
+	req, _ := http.NewRequest("GET", APIBasePath+"/notes", nil)
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	if body := res.Body.String(); body == "[]" {
+		t.Errorf("Expected NON empty payload (array). Got %s", body)
+	}
+
+	expected := []model.Note{model.Note{ID: 1, Title: "sample title", Body: "sample body"}}
+	var actual []model.Note
+	err := json.Unmarshal(res.Body.Bytes(), &actual)
+
+	if err != nil || actual == nil {
+		t.Errorf("Expected Non empty array. Got %s", err)
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected '%v' but got '%v'", expected, actual)
+	}
+}
+
+func TestGetNonExistenNote(t *testing.T) {
+	req, _ := http.NewRequest("GET", APIBasePath+"/notes/42", nil)
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, res.Code)
+	var m map[string]string
+	json.Unmarshal(res.Body.Bytes(), &m)
+	if m["error"] != respositories.NoteNotFoundMsg {
+		t.Errorf("Expected the 'error' key of the response to be set to '%s'. Got '%s'", respositories.NoteNotFoundMsg, m["error"])
+	}
+}
+
+func TestCreateNote(t *testing.T) {
+	payload := []byte(`{"title": "sample title", "body": "sample body"}`)
+
+	req, _ := http.NewRequest("POST", APIBasePath+"/notes", bytes.NewBuffer(payload))
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	loc := res.HeaderMap.Get("Location")
+	if loc != APIBasePath+"/notes/2" {
+		t.Errorf("Expected the '"+APIBasePath+"/notes/2' in header 'Location'. Got '%s'", loc)
+	}
+}
+
+func TestGetNote(t *testing.T) {
+	expected := model.Note{ID: 1, Title: "sample title", Body: "sample body"}
+	req, _ := http.NewRequest("GET", APIBasePath+"/notes/1", nil)
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	var actual model.Note
+	json.Unmarshal(res.Body.Bytes(), &actual)
+	compareNotes(expected, actual, t)
+}
+
+func TestUpdateNote(t *testing.T) {
+	expectedNote := model.Note{ID: 1, Title: "sample updated title", Body: "sample updated body"}
+	payload, _ := json.Marshal(expectedNote)
+
+	req, _ := http.NewRequest("PUT", APIBasePath+"/notes/1", bytes.NewBuffer(payload))
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	var actual bool
+	json.Unmarshal(res.Body.Bytes(), &actual)
+	if actual != true {
+		t.Errorf("Expected the 'true' but got '%v'", actual)
+	}
+
+	req, _ = http.NewRequest("GET", APIBasePath+"/notes/1", nil)
+	res = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	var actualNote model.Note
+	json.Unmarshal(res.Body.Bytes(), &actualNote)
+	compareNotes(expectedNote, actualNote, t)
+}
+
+func TestDeleteNote(t *testing.T) {
+
+	req, _ := http.NewRequest("DELETE", APIBasePath+"/notes/1", nil)
+	res := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+	var actual bool
+	json.Unmarshal(res.Body.Bytes(), &actual)
+	if actual != true {
+		t.Errorf("Expected the 'true' but got '%v'", actual)
+	}
+
+	req, _ = http.NewRequest("GET", APIBasePath+"/notes/1", nil)
+	res = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, res.Code)
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+func prepareNotes(nr respositories.NoteRepository) {
+	nr.CreateNote(model.Note{Title: "sample title", Body: "sample body"})
+}
+func compareNotes(expected, actual model.Note, t *testing.T) {
+	if expected.ID != actual.ID {
+		t.Errorf("Expected ID %d. Got %d\n", expected.ID, actual.ID)
+	}
+	if expected.Title != actual.Title {
+		t.Errorf("Expected Title %s. Got %s\n", expected.Title, actual.Title)
+	}
+	if expected.Body != actual.Body {
+		t.Errorf("Expected Body %s. Got %s\n", expected.Body, actual.Body)
+	}
+}
--- respositories/repository_test.go
+++ respositories/repository_test.go
@@ -1,11 +1,7 @@
 package respositories
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"os"
-	"reflect"
 	"testing"
 
 	"github.com/alex-bezverkhniy/go-notes-api/model"
@@ -13,38 +9,18 @@ import (
 )
 
 var noteRepository NoteRepository
+var sampleNote = model.Note{ID: 1, Title: "sample title", Body: "sample body"}
 
 func TestMain(m *testing.M) {
-	noteRepository = NoteRepository{DB: openConnection("gonotes", "1Q2w3e4r", "gonotes")}
+	noteRepository = NoteRepository{DB: OpenDBConnection("gonotes", "1Q2w3e4r", "gonotes")}
 
-	ensureNotesTableExists()
+	//noteRepository.EnsureNotesTableExists()
+	prepareNotes(noteRepository)
 	code := m.Run()
-	clearNotesTable()
+	noteRepository.ClearNotesTable()
 
 	os.Exit(code)
 
-}
-
-func ensureNotesTableExists() {
-	if _, err := noteRepository.DB.Exec(model.NoteDDL); err != nil {
-		log.Print(err)
-	} else {
-		log.Fatal("Table Note is not exist!")
-	}
-}
-
-func clearNotesTable() {
-	noteRepository.DB.Exec("DELETE FROM notes")
-	noteRepository.DB.Exec("ALTER TABLE notes AUTO_INCREMENT = 1")
-}
-
-func openConnection(user, password, dbname string) *sql.DB {
-	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
-	db, err := sql.Open("mysql", connectionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
 }
 
 func TestNoteRepository_CreateNote(t *testing.T) {
@@ -55,7 +31,7 @@ func TestNoteRepository_CreateNote(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{"happy", model.Note{0, "sample note title", "sample note body"}, 1, false},
+		{"happy", sampleNote, 2, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -74,16 +50,17 @@ func TestNoteRepository_CreateNote(t *testing.T) {
 func TestNoteRepository_UpdateNote(t *testing.T) {
 	tests := []struct {
 		name    string
+		id      int
 		note    model.Note
 		want    bool
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{"happy", model.Note{0, "sample note title", "sample note body"}, true, false},
+		{"happy", 1, sampleNote, true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := noteRepository.UpdateNote(1, tt.note)
+			got, err := noteRepository.UpdateNote(tt.id, tt.note)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NoteRepository.UpdateNote() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -103,7 +80,7 @@ func TestNoteRepository_GetNote(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{"happy", 1, model.Note{1, "sample note title", "sample note body"}, false},
+		{"happy", 1, sampleNote, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -128,7 +105,7 @@ func TestNoteRepository_GetNotes(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{"happy", 1, 1, []model.Note{model.Note{1, "sample note title", "sample note body"}}, false},
+		{"happy", 0, 10, []model.Note{sampleNote}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -137,9 +114,7 @@ func TestNoteRepository_GetNotes(t *testing.T) {
 				t.Errorf("NoteRepository.GetNotes() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NoteRepository.GetNotes() = %v, want %v", got, tt.want)
-			}
+			compareNotes(tt.want, got, t)
 		})
 	}
 }
@@ -165,5 +140,26 @@ func TestNoteRepository_DeleteNote(t *testing.T) {
 				t.Errorf("NoteRepository.DeleteNote() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func prepareNotes(nr NoteRepository) {
+	nr.CreateNote(sampleNote)
+}
+func compareNotes(expecteds, actuals []model.Note, t *testing.T) {
+	for i, expected := range expecteds {
+		compareNote(expected, actuals[i], t)
+	}
+}
+
+func compareNote(expected, actual model.Note, t *testing.T) {
+	if expected.ID != actual.ID {
+		t.Errorf("Expected ID '%d'. Got '%d'\n", expected.ID, actual.ID)
+	}
+	if expected.Title != actual.Title {
+		t.Errorf("Expected Title '%s'. Got '%s'\n", expected.Title, actual.Title)
+	}
+	if expected.Body != actual.Body {
+		t.Errorf("Expected Body '%s'. Got '%s'\n", expected.Body, actual.Body)
 	}
 }
